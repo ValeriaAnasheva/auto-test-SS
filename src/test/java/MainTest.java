e89b1143b9e1a367c30b67f145bb8fce704d1ad5
ValeriaAnasheva <valeria.mazur96@gmail.com>
2024-01-20 15:41:44 +0400

    use Page Object Model

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -2,82 +2,116 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class MainTest {
-    private WebDriver driver;
+
+public class MainTest {private WebDriver driver;
+
+    @FindBy(id = "firstName")
+    private WebElement firstNameInput;
+
+    @FindBy(id = "lastName")
+    private WebElement lastNameInput;
+
+    @FindBy(id = "userEmail")
+    private WebElement emailInput;
+
+    @FindBy(xpath = "//*[@id='genterWrapper']/div[2]/div[2]/label")
+    private WebElement genderRadio;
+
+    @FindBy(id = "userNumber")
+    private WebElement mobileInput;
+
+    @FindBy(id = "dateOfBirthInput")
+    private WebElement dobInput;
+
+    @FindBy(className = "react-datepicker__month")
+    private WebElement calendar;
+
+    @FindBy(id = "subjectsInput")
+    private WebElement subjectsInput;
+
+    @FindBy(id = "uploadPicture")
+    private WebElement uploadButton;
+
+    @FindBy(id = "currentAddress")
+    private WebElement addressInput;
+
+    @FindBy(id = "state")
+    private WebElement stateDropdown;
+
+    @FindBy(xpath = "//div[text()='NCR']")
+    private WebElement stateOption;
+
+    @FindBy(id = "city")
+    private WebElement cityDropdown;
+
+    @FindBy(xpath = "//div[text()='Delhi']")
+    private WebElement cityOption;
+
+    @FindBy(css = "#submit.btn.btn-primary")
+    private WebElement submitButton;
 
     @BeforeTest
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
         driver = new ChromeDriver();
+        PageFactory.initElements(driver, this);
     }
 
     @Test
     public void fillFormTest() {
         driver.get("https://demoqa.com/automation-practice-form");
 
-        // First Name
-        WebElement firstNameInput = driver.findElement(By.id("firstName"));
-        firstNameInput.sendKeys("Lera");
+        //First Name
+        firstNameInput.sendKeys("Valeria");
 
-        // Last Name
-        WebElement lastNameInput = driver.findElement(By.id("lastName"));
-        lastNameInput.sendKeys("Urev");
+        //Last Name
+        lastNameInput.sendKeys("Lera");
 
-        // Email
-        WebElement emailInput = driver.findElement(By.id("userEmail"));
-        emailInput.sendKeys("valer.aca@emailol.com");
+        //Email
+        emailInput.sendKeys("Lera.valera@xmail.com");
 
-        // Gender
-        WebElement genderRadio = driver.findElement(By.xpath("//*[@id=\"genterWrapper\"]/div[2]/div[2]/label"));
+        //Gender
         genderRadio.click();
 
-        // Mobile
-        WebElement mobileInput = driver.findElement(By.id("userNumber"));
+        //Mobile
         mobileInput.sendKeys("8800003232");
 
-        // Date of birth
-        WebElement dobInput = driver.findElement(By.id("dateOfBirthInput"));
+        //Date of birth
         dobInput.click();
-        WebElement calendar = driver.findElement(By.className("react-datepicker__month"));
         WebElement date = calendar.findElement(By.xpath("//div[text()='15']"));
         date.click();
 
-        // Subjects
-        WebElement subjectsInput = driver.findElement(By.id("subjectsInput"));
+        //Subjects
         subjectsInput.sendKeys("Maths");
 
-        // Picture
-        WebElement uploadButton = driver.findElement(By.id("uploadPicture"));
+        //Picture
         uploadButton.sendKeys("C:\\Users\\Valeriya\\IdeaProjects\\auto-test -SS\\src\\main\\resources\\imgSS.png");
 
         //Current Address
-        WebElement addressInput = driver.findElement(By.id("currentAddress"));
-        addressInput.sendKeys("123 Street, City");
+        addressInput.sendKeys("234 Red Square, Moscow");
 
-        //elect State
-        WebElement stateDropdown = driver.findElement(By.id("state"));
+        //Select State
         stateDropdown.click();
-        WebElement stateOption = driver.findElement(By.xpath("//div[text()='NCR']"));
         stateOption.click();
 
-        // Select City
-        WebElement cityDropdown = driver.findElement(By.id("city"));
+        //Select City
         cityDropdown.click();
-        WebElement cityOption = driver.findElement(By.xpath("//div[text()='Delhi']"));
         cityOption.click();
 
-        // Нажать Submit
-        WebDriverWait wait = new WebDriverWait(driver, 5L);
-        WebElement submitButton = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#submit.btn-primary")));
+        //Submit
+        WebDriverWait waiit = new WebDriverWait(driver, 10L);
+        waiit.until(ExpectedConditions.elementToBeClickable(submitButton));
         submitButton.click();
 
-        wait = new WebDriverWait(driver, 2L);
+        WebDriverWait wait = new WebDriverWait(driver, 15L);
         wait.until(ExpectedConditions.urlToBe("https://demoqa.com/automation-practice-form"));
 
     }
